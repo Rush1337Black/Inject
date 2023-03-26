a44ff5c28a2971426304d2c5044cac028356bbf2
Rush1337Black <128978324+Rush1337Black@users.noreply.github.com>
2023-03-26 18:01:16 +0300

    Refactor

--- InjectR32/Internal.cpp
+++ InjectR32/Internal.cpp
@@ -2,49 +2,53 @@
 
 #include "library.h"
 
-class Internal
-{
+class Internal{
+
 	public:
 
-		LPVOID GetNTOpenFile()
-		{
+		LPVOID GetNTOpenFile(){
+
 			return GetProcAddress(LoadLibraryW(L"ntdll"), "NtOpenFile");
+
 		}
 
+		bool CheckFileExist(const char* name){
 
-		bool CheckFileExist(const char* name)
-		{
-			if (FILE* file = fopen(name, "r"))
-			{
+			if (FILE* file = fopen(name, "r")){
+
 				fclose(file);
 				return true;
+
 			}
 			return false;
 		}
 
-		bool ExecuteBypass(HANDLE hProcess)
-		{
-			if (GetNTOpenFile())
-			{
+		bool ExecuteBypass(HANDLE hProcess){
+
+			if (GetNTOpenFile()){
+
 				char originalBytes[5];
 				memcpy(originalBytes, GetNTOpenFile(), 5);
-				if (WriteProcessMemory(hProcess, GetNTOpenFile(), originalBytes, 5, NULL))
-				{
+
+				if (WriteProcessMemory(hProcess, GetNTOpenFile(), originalBytes, 5, NULL)){
+
 					std::cout << "VAC Bypassed.." << std::endl;
 					return TRUE;
+
 				}
 			}
 			return FALSE;
 		}
 
-		bool Backup(HANDLE hProcess)
-		{
-			if (GetNTOpenFile())
-			{
+		bool Backup(HANDLE hProcess){
+
+			if (GetNTOpenFile()){
+
 				char Orig[5];
 				memcpy(Orig, GetNTOpenFile(), 5);
 				WriteProcessMemory(hProcess, GetNTOpenFile(), Orig, 0, 0);
 				return TRUE;
+
 			}
 			return FALSE;
 		}
